5fcc7557d0f31dfeb707395a19b9b550b7fd7f75
looted232 <xynconnect@gmail.com>
2026-03-19 10:59:00 -0500

    Update loadstring calls for game scripts

--- starx.lua
+++ starx.lua
@@ -2,11 +2,11 @@ local placeId = game.PlaceId
 
 -- SAB script (ID: 109983668079237)
 if placeId == 109983668079237 then
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/IZ08K8jDRd5mxYT3/raw"))()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ercBjVA7eAQyMa8z/raw"))()
 
 -- Escape Tsunami script (ID: 131623223084840)
 elseif placeId == 131623223084840 then
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/IZ08K8jDRd5mxYT3/raw"))()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/ercBjVA7eAQyMa8z/raw"))()
 
 else
     print("Unsupported game")
