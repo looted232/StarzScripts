88bb5f60866ad5e910c828f596d353641dc15a01
looted232 <xynconnect@gmail.com>
2026-05-16 22:44:53 -0500

    Update zero2.lua

--- zero2.lua
+++ zero2.lua
@@ -1,2 +1,2 @@
 --- Do Not Use Alt!!
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/X87Tjj5syhcwQlHd/raw"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/O63oWkqMcybKS96V/raw"))()
